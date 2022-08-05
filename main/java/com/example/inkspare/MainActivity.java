f76097a9ce6541d9fdc139e31e7ebe5505aa0a3d
poison-pen <103902575+poisonpen@users.noreply.github.com>
2022-08-05 18:23:39 -0400

    Add files via upload

---
+++ main/java/com/example/inkspare/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.inkspare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
+
+
+// if the user's id thingy matches one in their database, they can continue. Otherwise, they need to make an account or claim an account.
